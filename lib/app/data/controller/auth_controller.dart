409209dfdead49ff1ab590c69fbccc109ec95eb9
Dapi45 <daffifadil45@gmail.com>
2022-08-12 20:21:08 +0700

    Hosting

--- lib/app/data/controller/auth_controller.dart
+++ lib/app/data/controller/auth_controller.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:uas_app/app/routes/app_pages.dart';
 
 class AuthController extends GetxController {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  UserCredential? _userCredential;
   Future signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -17,15 +21,40 @@ class AuthController extends GetxController {
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
+
     print(googleUser!.email);
     // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance
+    await auth
         .signInWithCredential(credential)
-        .then((value) => Get.offAllNamed(Routes.HOME));
+        .then((value) => _userCredential = value);
+
+    // firebase
+    CollectionReference users = firestore.collection('users');
+
+    final cekuser = await users.doc(googleUser.email).get();
+    if (!cekuser.exists) {
+      users.doc(googleUser.email).set({
+        'uid': _userCredential!.user!.uid,
+        'name': googleUser.displayName,
+        'email': googleUser.email,
+        'photo': googleUser.photoUrl,
+        'creadAt': _userCredential!.user!.metadata.creationTime.toString(),
+        'lastloginAt':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
+        // 'list_cari': [D,DA,DAF,DAFF,DAFFI],
+      });
+    } else {
+      users.doc(googleUser.email).update({
+        'lastloginAt':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
+      });
+    }
+    Get.offAllNamed(Routes.HOME);
   }
 
   Future logout() async {
     await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
     Get.offAllNamed(Routes.LOGIN);
   }
 }
